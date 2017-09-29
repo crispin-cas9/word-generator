3b7962365150ae77b0cd7a8592e20544eb7094aa
Finley <starkwafie@gmail.com>
2017-09-29 15:19:07 -0700

    now generates syllables on their own
    but basically ignores phonotactics

--- c_words.py
+++ c_words.py
@@ -1,4 +1,4 @@
-# caemattos
+# caemattos word generator
 # ae = ash; ee = schwa
 
 import random as rd
@@ -6,27 +6,43 @@ import random as rd
 vowels = ["a", "ae", "e", "ee", "i", "o", "u"]
 consonants = ["d", "dd", "c", "cc", "f", "ff", "h", "hh", "l", "ll", "m", "n", "nn", "p", "pp", "r", "rr", "s", "ss", "t", "tt", "y", "yy", "z", "zz"]
 
+
+def gen_syll():
+
+	syll = ""
+
+	# is there an onset? 0 = no; 1 = yes
+	
+	is_onset = rd.randint(0,1)
+	coda_num = rd.randint(1,2)
+
+	if is_onset == 1:
+		syll = syll + rd.choice(consonants)
+	
+	# nucleus
+	
+	syll = syll + rd.choice(vowels)
+	
+	# coda. fix later!!
+	
+	syll = syll + rd.choice(consonants)
+	
+	if coda_num == 2:
+		syll = syll + rd.choice(consonants)
+	
+	return syll
+
+
 def generate():
-
-	length = rd.randint(2,5)
-
-	l = length - 1
+	
 	word = ""
-
-	# is there a vowel at the beginning?
-	cfirst = rd.randint(0,1)
-
-	if cfirst == 1:
-		x = rd.choice(consonants)
-		word = word + x
-
-	for x in range(0,l):
-		x = rd.choice(vowels)
-		word = word + x
-		x = rd.choice(consonants)
-		word = word + x
+	syllnum = rd.randint(1,4)
+	
+	for x in range(syllnum):
+		word = word + gen_syll()
 	
 	return word
+
 
 for y in range(0,11):
 	print generate()

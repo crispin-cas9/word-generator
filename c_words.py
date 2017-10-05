013cca1d86b7f5791eb7500cb30be66ef010257d
Finley <starkwafie@gmail.com>
2017-10-05 14:22:35 -0700

    goals

--- c_words.py
+++ c_words.py
@@ -1,5 +1,11 @@
 # caemattos word generator
-# ae = ash (no schwas yet...)
+# ae = ash
+
+# some goals:
+# make probability of some letters higher
+# have a dictionary that classes consonants as fricatives or stops (or other)
+# make it less likely for one sound to be repeated throughout a word
+# if the last syllable ends with two consonants, make it so that the next syllable has no onset (starts with a vowel)
 
 # important! in a syllable: onset = starting consonants, nucleus = middle vowels, coda = ending consonants
 
@@ -9,6 +15,10 @@ def gen_syll():
 
 	vowels = ["a", "ae", "e", "i", "o", "u"]
 	consonants = ["d", "dd", "c", "cc", "f", "ff", "h", "hh", "l", "ll", "m", "n", "nn", "p", "pp", "r", "rr", "s", "ss", "t", "tt", "y", "yy", "z", "zz"]
+	
+	# I don't necessary classify consonants correctly here -- for example, LL is not a fricative and Y is, but I'm classifying them this way to make it all easier for myself. 
+	
+	consonants = {"d": ["fric", 1], "dd": "fric", "c": "stop", "cc": "stop", "f": "fric", "ff": "fric", "h": "other", "hh": "other", "l": "other", "ll": "fric", "m": "other", "n": "other", "nn": "other", "p": "stop", "pp": "stop", "r": "fric", "rr": "other", "s": "fric", "ss": "fric", "t": "stop", "tt": "stop", "y": "other", "yy": "other", "z": "fric", "zz": "fric"}
 	
 	frics = ["d", "dd", "f", "ff", "ll", "r", "s", "ss", "z", "zz"]
 	stops = ["c", "cc", "p", "pp", "t", "tt"]

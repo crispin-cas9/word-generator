4c1dc27453988c514611adc55270c02ea0858348
Finley <starkwafie@gmail.com>
2017-10-05 13:30:31 -0700

    better phonotactics

--- c_words.py
+++ c_words.py
@@ -1,20 +1,25 @@
 # caemattos word generator
-# ae = ash; ee = schwa
+# ae = ash (no schwas yet...)
+
+# important! in a syllable: onset = starting consonants, nucleus = middle vowels, coda = ending consonants
 
 import random as rd
 
-vowels = ["a", "ae", "e", "ee", "i", "o", "u"]
-consonants = ["d", "dd", "c", "cc", "f", "ff", "h", "hh", "l", "ll", "m", "n", "nn", "p", "pp", "r", "rr", "s", "ss", "t", "tt", "y", "yy", "z", "zz"]
-
-
 def gen_syll():
+
+	vowels = ["a", "ae", "e", "i", "o", "u"]
+	consonants = ["d", "dd", "c", "cc", "f", "ff", "h", "hh", "l", "ll", "m", "n", "nn", "p", "pp", "r", "rr", "s", "ss", "t", "tt", "y", "yy", "z", "zz"]
+	
+	frics = ["d", "dd", "f", "ff", "ll", "r", "s", "ss", "z", "zz"]
+	stops = ["c", "cc", "p", "pp", "t", "tt"]
 
 	syll = ""
 
 	# is there an onset? 0 = no; 1 = yes
-	
 	is_onset = rd.randint(0,1)
-	coda_num = rd.randint(1,2)
+	
+	# are there two consonants in the coda? picking from 4 numbers to make the probability of 2 consonants smaller
+	coda_num = rd.randint(1,6)
 
 	if is_onset == 1:
 		syll = syll + rd.choice(consonants)
@@ -23,11 +28,20 @@ def gen_syll():
 	
 	syll = syll + rd.choice(vowels)
 	
-	# coda. fix later!!
+	no = ["h", "hh", "y", "yy"]
 	
-	syll = syll + rd.choice(consonants)
+	for letter in no:
+		consonants.remove(letter)
+	
+	# coda
 	
 	if coda_num == 2:
+		fric = rd.choice(frics)
+		for letter in stops:
+			if len(letter) != len(fric) and letter != "r":
+				stops.remove(letter)
+		syll = syll + fric + rd.choice(stops)
+	else:
 		syll = syll + rd.choice(consonants)
 	
 	return syll
@@ -36,7 +50,7 @@ def gen_syll():
 def generate():
 	
 	word = ""
-	syllnum = rd.randint(1,4)
+	syllnum = rd.randint(1,3)
 	
 	for x in range(syllnum):
 		word = word + gen_syll()
